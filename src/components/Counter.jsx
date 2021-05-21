44e366623c2fe5348efb5474fc31293611e58ede
arturconrado <conraderp@gmail.com>
2021-05-20 21:03:04 -0300

    aprendendo conceito de estado, usando use state, e o setVARIAVEL

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
+
 export function Counter() {
+    const [counter, setCounter] = useState(0);
+    function increment(){
+        setCounter(counter + 1);
+    }
+
     return (
         <div>
             <h2>
-                0
+                {counter}
             </h2>
 
-            <button type="button">Add</button>
+            <button type="button" onClick={increment}>
+                Add
+            </button>
         </div>
     );
 }
